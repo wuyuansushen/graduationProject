0c8afc6a70ced1e7d4ee8f73ede0955cf34dd19d
wuyuansushen <43259764+wuyuansushen@users.noreply.github.com>
2022-03-29 00:23:51 +0800

    Add View...OK

--- graduation/graduation/App.xaml.cs
+++ graduation/graduation/App.xaml.cs
@@ -10,7 +10,7 @@ namespace graduation
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new AppShell();
         }
 
         protected override void OnStart()
--- graduation/graduation/Views/DownloadPage.xaml.cs
+++ graduation/graduation/Views/DownloadPage.xaml.cs
@@ -1,16 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Xamarin.Forms;
 
-namespace graduation
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace graduation.Views
 {
-    public partial class MainPage : ContentPage
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class DownloadPage : ContentPage
     {
-        public MainPage()
+        public DownloadPage()
         {
             InitializeComponent();
         }
