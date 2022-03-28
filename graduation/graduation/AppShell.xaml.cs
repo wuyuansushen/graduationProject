1ce397f8026f208a37cc10513ce47c171ec34e06
wuyuansushen <43259764+wuyuansushen@users.noreply.github.com>
2022-03-28 22:32:34 +0800

    UI

---
+++ graduation/graduation/AppShell.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace graduation
+{
+    public partial class AppShell:Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ graduation/graduation/Services/SendHash.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net.Http;
+
+namespace graduation.Services
+{
+    internal class SendHash
+    {
+        public HttpClient Client { get;}
+        public SendHash()
+        {
+            Client = new HttpClient();
+        }
+    }
+}
