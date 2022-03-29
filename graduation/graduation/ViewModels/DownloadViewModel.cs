864c8283d7cf221c12ca762d8e9cbfb20bc751a5
wuyuansushen <43259764+wuyuansushen@users.noreply.github.com>
2022-03-30 00:59:56 +0800

    Add empty viewmodel...OK

---
+++ graduation/graduation/ViewModels/DownloadViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin;
+using Xamarin.Forms;
+
+using System.ComponentModel;
+
+namespace graduation.ViewModels
+{
+    internal class DownloadViewModel:INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
