00c18977c660e35bbfb3f637e562cf8a25c3a4f1
wuyuansushen <43259764+wuyuansushen@users.noreply.github.com>
2022-03-29 22:58:50 +0800

    remove Grid

---
+++ graduation/graduation/Behaviors/SendHashBehavior.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Xamarin;
+using Xamarin.Forms;
+
+namespace graduation.Behaviors
+{
+    internal class SendHashBehavior:Behavior<Button>
+    {
+        protected override void OnAttachedTo(Button bindable)
+        {
+            base.OnAttachedTo(bindable);
+        }
+
+        protected override void OnDetachingFrom(Button bindable)
+        {
+            base.OnDetachingFrom(bindable);
+        }
+    }
+}
