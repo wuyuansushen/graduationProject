e0c263aa0a1c66b79dda40dedc9f768cce3d78e0
wuyuansushen <43259764+wuyuansushen@users.noreply.github.com>
2022-03-31 14:10:28 +0800

    Remove demo INotifyPropertyChanged

--- graduation/graduation/ViewModels/DownloadViewModel.cs
+++ graduation/graduation/ViewModels/DownloadViewModel.cs
@@ -15,6 +15,7 @@ namespace graduation.ViewModels
             get; set;
         }
 
+        /*
         private string _hash = String.Empty;
         public string Hash { get { return _hash; } 
             //Every an short time modification in Entry prompt(value is Text) will call this setter immediatly.
@@ -22,7 +23,7 @@ namespace graduation.ViewModels
                 if (_hash != value)
                 {
                     //_hash no change，Still be empty so it will not change when clear to empty
-                    //_hash = value;
+                    _hash = value;
                     Title = value;
                     //Invoke event to nofity must be after the assigned.
                     PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Title"));
@@ -31,6 +32,7 @@ namespace graduation.ViewModels
                     //and change the display in XAML.
                 }
             } }
+        */
         public DownloadViewModel()
         {
             Title = "下载页";
@@ -38,7 +40,6 @@ namespace graduation.ViewModels
         public DownloadViewModel(string title)
         {
             Title =title;
-            Hash = title;
         }
         public event PropertyChangedEventHandler PropertyChanged;
     }
