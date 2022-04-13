9388e8c1753a8b2e3da76395644341cd2c658e9b
wuyuansushen <43259764+wuyuansushen@users.noreply.github.com>
2022-04-13 14:19:34 +0800

    Open in browser...OK

--- graduation/graduation/ViewModels/TorrentViewModel.cs
+++ graduation/graduation/ViewModels/TorrentViewModel.cs
@@ -2,10 +2,24 @@
 using System.Collections.Generic;
 using System.Text;
 using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Essentials;
+using System.Windows.Input;
 
 namespace graduation.ViewModels
 {
-    public class TorrentViewModel
+    public class TorrentViewModel:INotifyPropertyChanged
     {
+        public ICommand OpenBrowserCommand { get;private set; }
+        private const string RepoUrl = @"https://fiveelementgod.xyz/repo/";
+        public TorrentViewModel()
+        {
+            OpenBrowserCommand = new Command(execute:
+                async () =>
+                {
+                    await Browser.OpenAsync(RepoUrl);
+                });
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
     }
 }
